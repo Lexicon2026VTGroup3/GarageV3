6237420819bbd2ff0bb79e6bc2048c39668dca33
86854402+YeoStrong@users.noreply.github.com <86854402+YeoStrong@users.noreply.github.com>
2026-08-09 19:29:58 +0200

    [US04-2] Add My Vehicles Create function

--- GarageV3/Controllers/MyVehiclesController.cs
+++ GarageV3/Controllers/MyVehiclesController.cs
@@ -1,10 +1,12 @@
 
 using GarageV3.Data;
 using GarageV3.Models.Entities;
+using GarageV3.Models.Enums;
 using GarageV3.ViewModels;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 
 [Authorize]
@@ -65,7 +67,17 @@ public class MyVehiclesController : Controller
     // GET: VEHICLES/Create
     public IActionResult Create()
     {
-        return View();
+        var viewModel = new ParkedVehicleFormViewModel
+        {
+            VehicleTypes = Enum.GetValues(typeof(VehicleType))
+                              .Cast<VehicleType>()
+                              .Select(t => new SelectListItem
+                              {
+                                  Value = t.ToString(),
+                                  Text = t.ToString()
+                              })
+        };
+        return View(viewModel);
     }
 
     // POST: VEHICLES/Create
@@ -73,31 +85,62 @@ public class MyVehiclesController : Controller
     // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
     [HttpPost]
     [ValidateAntiForgeryToken]
-    public async Task<IActionResult> Create([Bind("Id,VehicleTypeRefId,VehicleTypeRef,OwnerId,Owner,RegistrationNumber,Color,Brand,Model,NumberOfWheels,ArrivalTime,AssignedSpotNumber")] Vehicle vehicle)
+    public async Task<IActionResult> Create(ParkedVehicleFormViewModel viewModel)
     {
+        viewModel.RegistrationNumber = viewModel.RegistrationNumber.Trim().ToUpper();
+
+        var vehicleTypeEntity = await _context.VehicleTypes
+            .FirstOrDefaultAsync(vt => vt.EnumValue == viewModel.VehicleType);
+
+        if (vehicleTypeEntity == null)
+        {
+            ModelState.AddModelError("VehicleType", "Selected vehicle type is not recognized.");
+        }
+
+        var userId = _userManager.GetUserId(User);
+        if (userId == null)
+        {
+            ModelState.AddModelError(string.Empty, "You must be logged in to register a vehicle.");
+        }
+
         if (ModelState.IsValid)
         {
-            _context.Add(vehicle);
-            await _context.SaveChangesAsync();
-            return RedirectToAction(nameof(Index));
-        }
-        return View(vehicle);
-    }
+            try
+            {
+                var vehicle = new Vehicle
+                {
+                    VehicleTypeRefId = vehicleTypeEntity!.Id,
+                    OwnerId = userId!,
+                    RegistrationNumber = viewModel.RegistrationNumber,
+                    Color = viewModel.Color ?? string.Empty,
+                    Brand = viewModel.Brand ?? string.Empty,
+                    Model = viewModel.Model ?? string.Empty,
+                    NumberOfWheels = viewModel.NumberOfWheels.GetValueOrDefault(),
+                    ArrivalTime = DateTime.Now
+                };
 
-    // GET: VEHICLES/Edit/5
-    public async Task<IActionResult> Edit(int? id)
-    {
-        if (id == null)
-        {
-            return NotFound();
+                _context.Vehicles.Add(vehicle);
+                await _context.SaveChangesAsync();
+
+                TempData["SuccessMessage"] = $"Successfully registered vehicle {viewModel.RegistrationNumber}.";
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Could not register vehicle. Please check all fields.");
+                Console.WriteLine("DB ERROR: " + ex.Message);
+            }
         }
 
-        var vehicle = await _context.Vehicles.FindAsync(id);
-        if (vehicle == null)
-        {
-            return NotFound();
-        }
-        return View(vehicle);
+        viewModel.VehicleTypes = Enum.GetValues(typeof(VehicleType))
+            .Cast<VehicleType>()
+            .Select(v => new SelectListItem
+            {
+                Text = v.ToString(),
+                Value = v.ToString()
+            });
+
+        return View(viewModel);
     }
 
     // POST: VEHICLES/Edit/5
