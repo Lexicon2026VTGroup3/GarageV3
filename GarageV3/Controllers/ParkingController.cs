efe4764af4e35fc0d4ab75000b59aee6d0122164
Yangshan <yangshan.liu55@gmail.com>
2026-08-12 23:26:40 +0200

    Fix showing VehicleType based on DB data.

--- GarageV3/Controllers/ParkingController.cs
+++ GarageV3/Controllers/ParkingController.cs
@@ -61,6 +61,8 @@ namespace GarageV3.Controllers
                     SessionId = ps.Id,
                     RegistrationNumber = (ps.Vehicle != null && ps.Vehicle.RegistrationNumber != null) ? ps.Vehicle.RegistrationNumber : string.Empty,
                     VehicleTypeName = (ps.Vehicle != null && ps.Vehicle.VehicleTypeRef != null) ? ps.Vehicle.VehicleTypeRef.Name : "Unknown",
+                    VehicleTypeIcon = (ps.Vehicle != null && ps.Vehicle.VehicleTypeRef != null) ? ps.Vehicle.VehicleTypeRef.Icon : string.Empty,
+                    RequiredSpots = (ps.Vehicle != null && ps.Vehicle.VehicleTypeRef != null) ? ps.Vehicle.VehicleTypeRef.RequiredSpots : 1,
                     ParkingSpotId = (ps.ParkingSpot != null) ? ps.ParkingSpot.Id : -1,
                     ArrivalTime = ps.ArriveTime,
                     CheckOutTime = (ps.CheckOutTime != null) ? ps.CheckOutTime.Value : DateTime.MinValue,
